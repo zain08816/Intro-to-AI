b84d38054149df92fe8621d0d1d2c8b3d9bab3e2
cocoacitrus <cocoacitrus@localhost.localdomain>
2020-07-08 16:03:11 -0400

    maze generator ready. see comments in code

---
+++ projects/project_1/README.md
@@ -0,0 +1,7 @@
+# Intro to A.I.
+# Project 1
+
+notes:
+- comments by Michael are signed, "\s MBS"
+- added a few things to ./src/grid-world-generator.py; see comments. 
+- edited ../../.git/info/exclude; see comments.
---
+++ projects/project_1/src/tmp/mbs_scratch.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+'''
+This is simply "scratch paper" to test python modules and such. 
+I labeled it with my initials, in case you want to make something similar in the
+same directory.
+'''
+import sys
+
+print(type(sys.platform))
+print(sys.platform == 'linux')
+exit(0)
