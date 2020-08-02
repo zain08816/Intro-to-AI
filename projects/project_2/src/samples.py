d2aade1ea871a776d90def6fb64f718fab3a5192
Zain Ali <zain08816@gmail.com>
2020-08-01 21:10:49 -0600

    change back to python 2

--- projects/project_2/src/samples.py
+++ projects/project_2/src/samples.py
@@ -18,21 +18,21 @@ class Datum:
   """
   A datum is a pixel-level encoding of digits or face/non-face edge maps.
 
-  Digits are from the MNIST dataset and face images are from the
+  Digits are from the MNIST dataset and face images are from the 
   easy-faces and background categories of the Caltech 101 dataset.
-
-
-  Each digit is 28x28 pixels, and each face/non-face image is 60x74
+  
+  
+  Each digit is 28x28 pixels, and each face/non-face image is 60x74 
   pixels, each pixel can take the following values:
     0: no edge (blank)
     1: gray pixel (+) [used for digits only]
     2: edge [for face] or black pixel [for digit] (#)
-
+    
   Pixel data is stored in the 2-dimensional array pixels, which
   maps to pixels on a plane according to standard euclidean axes
   with the first dimension denoting the horizontal and the second
   the vertical coordinate:
-
+    
     28 # # # #      #  #
     27 # # # #      #  #
      .
@@ -43,10 +43,10 @@ class Datum:
      1 # # # #      #  #
      0 # # # #      #  #
        0 1 2 3 ... 27 28
-
+   
   For example, the + in the above diagram is stored in pixels[2][3], or
   more generally pixels[column][row].
-
+       
   The contents of the representation can be accessed directly
   via the getPixel and getPixels methods.
   """
@@ -59,21 +59,21 @@ class Datum:
     self.height = DATUM_HEIGHT
     self.width = DATUM_WIDTH
     if data == None:
-      data = [[' ' for i in range(DATUM_WIDTH)] for j in range(DATUM_HEIGHT)]
-    self.pixels = util.arrayInvert(convertToInteger(data))
-
+      data = [[' ' for i in range(DATUM_WIDTH)] for j in range(DATUM_HEIGHT)] 
+    self.pixels = util.arrayInvert(convertToInteger(data)) 
+    
   def getPixel(self, column, row):
     """
     Returns the value of the pixel at column, row as 0, or 1.
     """
     return self.pixels[column][row]
-
+      
   def getPixels(self):
     """
     Returns all pixels as a list of lists.
     """
-    return self.pixels
-
+    return self.pixels    
+      
   def getAsciiString(self):
     """
     Renders the data item as an ascii image.
@@ -84,18 +84,18 @@ class Datum:
       ascii = map(asciiGrayscaleConversionFunction, row)
       rows.append( "".join(ascii) )
     return "\n".join(rows)
-
+    
   def __str__(self):
     return self.getAsciiString()
-
+    
 
 
 # Data processing, cleanup and display functions
-
+    
 def loadDataFile(filename, n,width,height):
   """
   Reads n data images from a file and returns a list of Datum objects.
-
+  
   (Return less then n items if the end of file is encountered).
   """
   DATUM_WIDTH=width
@@ -109,7 +109,7 @@ def loadDataFile(filename, n,width,height):
       data.append(list(fin.pop()))
     if len(data[0]) < DATUM_WIDTH-1:
       # we encountered end of file...
-      print( "Truncating at %d examples (maximum)" % i)
+      print "Truncating at %d examples (maximum)" % i
       break
     items.append(Datum(data,DATUM_WIDTH,DATUM_HEIGHT))
   return items
@@ -118,12 +118,12 @@ import zipfile
 import os
 def readlines(filename):
   "Opens a file or reads it from the zip archive data.zip"
-  if(os.path.exists(filename)):
+  if(os.path.exists(filename)): 
     return [l[:-1] for l in open(filename).readlines()]
-  else:
+  else: 
     z = zipfile.ZipFile('data.zip')
     return z.read(filename).split('\n')
-
+    
 def loadLabelsFile(filename, n):
   """
   Reads n labels from a file and returns a list of integers.
@@ -135,7 +135,7 @@ def loadLabelsFile(filename, n):
         break
     labels.append(int(line))
   return labels
-
+  
 def asciiGrayscaleConversionFunction(value):
   """
   Helper function for display purposes.
@@ -145,8 +145,8 @@ def asciiGrayscaleConversionFunction(value):
   elif(value == 1):
     return '+'
   elif(value == 2):
-    return '#'
-
+    return '#'    
+    
 def IntegerConversionFunction(character):
   """
   Helper function for file reading.
@@ -156,7 +156,7 @@ def IntegerConversionFunction(character):
   elif(character == '+'):
     return 1
   elif(character == '#'):
-    return 2
+    return 2    
 
 def convertToInteger(data):
   """
@@ -178,12 +178,12 @@ def _test():
   items = loadDataFile("digitdata/trainingimages", n,28,28)
   labels = loadLabelsFile("digitdata/traininglabels", n)
   for i in range(1):
-    print( items[i])
-    print( items[i])
-    print(items[i].height)
-    print(items[i].width)
-    print( dir(items[i]))
-    print( items[i].getPixels())
+    print items[i]
+    print items[i]
+    print (items[i].height)
+    print (items[i].width)
+    print dir(items[i])
+    print items[i].getPixels()
 
 if __name__ == "__main__":
-  _test()
+  _test()  
